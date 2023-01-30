7969ae6ae1e779ea730dd598168c62e63cfe3ac5
Fabco <contact@fabco.tech>
2023-01-30 12:10:22 +0100

    added agEur on polygon and ethereum

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ const web3Polygon = new Web3("https://polygon-rpc.com");
 // Ethereum mainnet
 const USDT_TOKEN_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 const USDC_TOKEN_ADDRESS = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+const AGEUR_TOKEN_ADDRESS = "0x1a7e4e63778B4f12a199C062f3eFdD288afCBce8";
 
 // BNB Chain https://bscscan.com/tokens
 const BUSD_TOKEN_ADDRESS = "0xe9e7cea3dedca5984780bafc599bd69add087d56";
@@ -46,6 +47,7 @@ const USDT_TOKEN_ADDRESS_ON_BSC = "0x55d398326f99059ff775485246999027b3197955";
 // Polygon https://polygonscan.com/tokens
 const USDC_TOKEN_ADDRESS_ON_POLYGON = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
 const USDT_TOKEN_ADDRESS_ON_POLYGON = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
+const AGEUR_TOKEN_ADDRESS_ON_POLYGON = "0xE0B52e49357Fd4DAf2c15e02058DCE6BC0057db4";
 
 let addresses = [];
 if (getProcessArgv("--address")) {
@@ -97,12 +99,18 @@ const print = async (ethAddress) => {
     minABI,
     USDC_TOKEN_ADDRESS
   );
+  const ageurContract = new web3Ethereum.eth.Contract(
+    minABI,
+    AGEUR_TOKEN_ADDRESS
+  );
+
   const bscBUSDContract = new web3BSC.eth.Contract(minABI, BUSD_TOKEN_ADDRESS);
   const bscUSDTContract = new web3BSC.eth.Contract(minABI, USDT_TOKEN_ADDRESS_ON_BSC);
   const bscUSDCContract = new web3BSC.eth.Contract(minABI, USDC_TOKEN_ADDRESS_ON_BSC);
 
   const polygonUSDTContract = new web3Polygon.eth.Contract(minABI, USDT_TOKEN_ADDRESS_ON_POLYGON);
   const polygonUSDCContract = new web3Polygon.eth.Contract(minABI, USDC_TOKEN_ADDRESS_ON_POLYGON);
+  const polygonAGEURContract = new web3Polygon.eth.Contract(minABI, AGEUR_TOKEN_ADDRESS_ON_POLYGON);
 
   if (!balances[ethAddress]) balances[ethAddress] = {};
 
@@ -143,6 +151,19 @@ const print = async (ethAddress) => {
   logs += "USDC (Ethereum)      : " + usdcEthereum / 10 ** 6 + "\n";
   balances[ethAddress]['usdc-eth'] = usdcEthereum / 10 ** 6;
 
+  let agEurEthereum;
+  try {
+    agEurEthereum = await ageurContract.methods
+    .balanceOf(ethAddress)
+    .call();
+  } catch (err) {
+    console.log('Failed to get agEur Ethereum balance')
+    throw err;
+  }
+
+  logs += "agEur (Ethereum)     : " + agEurEthereum / 10 ** 18 + "\n";
+  balances[ethAddress]['ageur-eth'] = agEurEthereum / 10 ** 18;
+
   let matic;
   try {
     matic = await web3Polygon.eth.getBalance(ethAddress)
@@ -179,6 +200,19 @@ const print = async (ethAddress) => {
 
   logs += "USDC  (Polygon)      : " + usdcPolygon / 10 ** 6 + "\n";
   balances[ethAddress]['usdc-polygon'] = usdcPolygon / 10 ** 6;
+
+  let agEurPolygon;
+  try {
+    agEurPolygon = await polygonAGEURContract.methods
+    .balanceOf(ethAddress)
+    .call()
+  } catch (err) {
+    console.log('Failed to get agEur (Polygon) balance')
+    throw err;
+  }
+
+  logs += "agEur (Polygon)      : " + agEurPolygon / 10 ** 18 + "\n";
+  balances[ethAddress]['ageur-polygon'] = agEurPolygon / 10 ** 18;
 
   let bnb;
   try {
